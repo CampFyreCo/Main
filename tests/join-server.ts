648342fef8a2b8ca42e6e57e16060a9f572770f8
Donovan Daniels <dwddaniels262@gmail.com>
2021-03-09 04:46:00 -0600

    don't need these

--- tests/join-server.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../src/db/models";
-
-process.nextTick(async () => {
-	const u = await User.getUser({
-		handle: "admin"
-	});
-	if (u === null) throw new TypeError("null");
-});
